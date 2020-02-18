1ba651de330606d9a24f9dbadc45d9b18e74e0a9
Mhd.Fadly Hasan <37266671+mhdfadlyhasan@users.noreply.github.com>
2020-02-18 23:07:27 +0700

    Setting JExcel, and 25% progress on the works
    i need to watch some vtuber first

---
+++ src/components/views/Jexcel.vue
@@ -0,0 +1,55 @@
+<template>
+<div id="spreadsheet">
+    <div id="tabel">
+    <input type="button" @click="logdata" value="Send Data">
+    </div>
+</div>
+
+</template>
+<script>
+import jexcel from 'jexcel'
+import 'jexcel/dist/jexcel.css'
+import Vue from 'vue'
+// import axios from 'axios'
+
+var data = [
+  ['Jazz', 'Honda', '2019-02-12', '', true, '$ 2.000,00', '#777700'],
+  ['Civic', 'Honda', '2018-07-11', '', true, '$ 4.000,01', '#007777']
+]
+
+var vm = new Vue(
+  {
+    el: '#inputer',
+    data: data,
+    mounted: function() {
+      console.log('fetchdata disini')
+    }
+  })
+console.log(vm)
+
+var options = {
+  data: data,
+  allowToolbar: true,
+  columns: [
+    { type: 'text', title: 'Car', width: '120px' },
+    { type: 'dropdown', title: 'Make', width: '250px', source: [ 'Alfa Romeo', 'Audi', 'Bmw' ] },
+    { type: 'calendar', title: 'Available', width: '250px' },
+    { type: 'image', title: 'Photo', width: '120px' },
+    { type: 'checkbox', title: 'Stock', width: '80px' },
+    { type: 'numeric', title: 'Price', width: '100px', mask: '$ #.##,00', decimal: ',' }
+  ]
+}
+export default {
+  name: 'tabel',
+  data: data,
+  mounted: function () {
+    let spreadsheet = jexcel(this.$el, options)
+    Object.assign(this, spreadsheet)
+  },
+  methods:
+  {logdata: function() {
+    console.log('data dikirim')
+    console.log(data)
+  }}
+}
+</script>
--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,7 @@ import SettingView from './components/views/Setting.vue'
 import AccessView from './components/views/Access.vue'
 import ServerView from './components/views/Server.vue'
 import ReposView from './components/views/Repos.vue'
-
+import Jexcel from './components/views/Jexcel.vue'
 // Routes
 const routes = [
   {
@@ -57,6 +57,12 @@ const routes = [
         component: ReposView,
         name: 'Repository',
         meta: {description: 'List of popular javascript repos'}
+      },
+      {
+        path: 'Jexcel',
+        component: Jexcel,
+        name: 'Jexcel Table',
+        meta: {description: 'TUGAS-1'}
       }
     ]
   }, {
